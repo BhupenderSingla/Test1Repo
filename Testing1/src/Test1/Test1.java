87d7ffe35a890979a30bce2191a8fc0586f0ff8f
mrsin <mrsin@DESKTOP-V31TRCU>
2023-10-10 20:01:45 +0530

    Test2

---
+++ Testing1/src/Test1/Test1.java
@@ -0,0 +1,21 @@
+package Test1;
+
+public class Test1 {
+	
+	int a;
+
+	public void method()
+	{
+		
+		System.out.println("Testing 1 sucsess");
+		
+	}
+public static void main(String[] args) {
+	Test1 obj=new Test1();
+	
+	obj.method();
+	obj.a=23;
+	System.out.println(obj.a);
+}
+}
+

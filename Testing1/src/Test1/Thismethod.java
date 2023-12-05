36d68e4efcf11f71d4de930fa90eeee7684d122a
mrsin <mrsin@DESKTOP-V31TRCU>
2023-12-05 17:43:13 +0530

    This with method

---
+++ Testing1/src/Test1/Thismethod.java
@@ -0,0 +1,32 @@
+package Test1;
+
+public class Thismethod {
+
+	public void m1()
+	{
+		this.m3();
+		System.out.println("M1 method");
+		this.m2();
+		this.m4();
+	}
+	public void m2()
+	{
+		System.out.println("M2 Method");
+	}
+	public void m3()
+	{
+		System.out.println("M3 method");
+	}
+	public void m4()
+	{
+		System.out.println("M4 method");
+	}
+	public static void main(String[] args) {
+		
+		Thismethod ob=new Thismethod();
+		ob.m1();
+		
+		
+	}
+	
+}
